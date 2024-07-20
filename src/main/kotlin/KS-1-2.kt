264b35b41777a3dd9ca21939c52af45524547955
barne43 <danilatov63@gmail.com>
2024-07-20 15:12:22 +0400

    KS-1-2

---
+++ src/main/kotlin/KS-1-2.kt
@@ -0,0 +1,16 @@
+package org.example
+
+fun main(){
+    val count_Of_Orders = 75
+    val text_After_Payment = "Спасибо за покупку!"
+
+    println(count_Of_Orders)
+    println(text_After_Payment)
+
+    var count_Of_Worker = 2000
+
+//    println(count_Of_Worker)
+
+    --count_Of_Worker
+    println(count_Of_Worker)
+}
