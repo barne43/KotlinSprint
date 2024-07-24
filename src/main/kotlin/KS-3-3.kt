8b2c597294ac2aa31f67b59b1152c4fe7d281998
barne43 <danilatov63@gmail.com>
2024-07-24 21:50:24 +0400

    KS-3-3 refactor

--- src/main/kotlin/KS-3-3.kt
+++ src/main/kotlin/KS-3-3.kt
@@ -2,9 +2,11 @@
 fun main(){
     val num = 5
     val tabl = buildString {
-        for (i in 1..10) {
+        for (i in ONE_NUMBER..TEN_NUMBER) {
             append("$num * $i = ${num * i}\n")
         }
     }
     println(tabl)
 }
+const val ONE_NUMBER = 1
+const val TEN_NUMBER = 10
