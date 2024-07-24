16c3e7a7d45684c50e13c786f49bf4b0c14e0845
barne43 <danilatov63@gmail.com>
2024-07-24 12:47:55 +0400

    KS-2-1 refactor

--- src/main/kotlin/KS_2/KS-2-1.kt
+++ src/main/kotlin/KS_2/KS-2-1.kt
@@ -1,5 +1,11 @@
 package KS_2
 
-fun main(){
-    println((3f + 4f + 3f +5f) / 4f)
+fun main() {
+    val students = 4
+    val studentAssessment1 = 3f
+    val studentAssessment2 = 4f
+    val studentAssessment3 = 3f
+    val studentAssessment4 = 5f
+
+    println((studentAssessment1 + studentAssessment2 + studentAssessment3 + studentAssessment4) / students)
 }
