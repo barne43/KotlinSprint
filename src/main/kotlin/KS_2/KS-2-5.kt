a5089ce941ef2b006a412d37a2feb564e489dd86
barne43 <danilatov63@gmail.com>
2024-07-24 12:43:03 +0400

    KS-2-5 refactor

--- src/main/kotlin/KS_2/KS-2-5.kt
+++ src/main/kotlin/KS_2/KS-2-5.kt
@@ -2,10 +2,12 @@ package KS_2
 
 fun main(){
     val sum = 70000
-    val interestRate = 16.7 / 100
+    val interestRate = 16.7
+
+    val interestRateDel = interestRate / 100
     val throughYears = 20
 
-    val finalAmount = sum * Math.pow(1 + interestRate, throughYears.toDouble())
+    val finalAmount = sum * Math.pow(1 + interestRateDel, throughYears.toDouble())
 
     println("Размер вклада через $throughYears лет: ${String.format("%.3f", finalAmount)}")
 }
