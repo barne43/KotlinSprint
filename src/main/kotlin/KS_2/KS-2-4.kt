f6028b39782253ecad1fe8f47b531c400942b8e4
barne43 <danilatov63@gmail.com>
2024-07-24 12:34:35 +0400

    KS-2-4 refactor

--- src/main/kotlin/KS_2/KS-2-4.kt
+++ src/main/kotlin/KS_2/KS-2-4.kt
@@ -4,10 +4,12 @@ fun main(){
     val cristalOre = 7
     val ironOre = 11
 
-    val bonus = 20 / 100.0
+    val bonus = 20
 
-    val bonusCristal = (cristalOre * bonus).toInt()
-    val bonusIron = (ironOre * bonus).toInt()
+    val bonusDel = bonus / 100.0
+
+    val bonusCristal = (cristalOre * bonusDel).toInt()
+    val bonusIron = (ironOre * bonusDel).toInt()
 
     println("Бонус к криталлической руде: $bonusCristal")
     println("Бонус к железной руде: $bonusIron")
