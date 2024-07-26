39cf65edb9b4799806e6cf8c82e60636c976dba7
barne43 <danilatov63@gmail.com>
2024-07-26 23:31:16 +0400

    KS-4-5

---
+++ src/main/kotlin/KS-4-5.kt
@@ -0,0 +1,22 @@
+fun main(){
+    print("Есть ли повреждения у корабля? (true/false): ")
+    val damageShip = readln().toBoolean()
+
+    print("Введите текущий состав корабля: ")
+    val crewSize = readln().toInt()
+
+    print("Введите количество ящиков с проввизией: ")
+    val provisionCount = readln().toInt()
+
+    print("Благоприятная ли погода? (true/false) ")
+    val isFavorableWeather = readln().toBoolean()
+
+    val canSail = ( !damageShip && crewSize in 55..70 && provisionCount < 50 && isFavorableWeather) ||
+            (damageShip && crewSize == 70 && provisionCount >= 50 && isFavorableWeather)
+
+    if (canSail){
+        println("Корабль может отправиться в плавание.")
+    } else {
+        println("Корабль не может отправиться в плавание.")
+    }
+}
