fa6e7f05782c44570bcc184a0e69eacd9a2e1711
barne43 <danilatov63@gmail.com>
2024-07-28 13:39:19 +0400

    KS-4-4 refactor

--- src/main/kotlin/KS-4-4.kt
+++ src/main/kotlin/KS-4-4.kt
@@ -1,15 +1,18 @@
 fun main(){
     val trainingDay = 5
 
-    val isArmsAndAbsDay = trainingDay % 2 != 0
-    val isLegsAndBackDay = trainingDay % 2 == 0
+    val isEven = trainingDay % 2 == 0
+
+    val exercisesForArmsAndAbs = !isEven
+    val exercisesForLegs  = isEven
+    val exercisesForBack = isEven
+    val exercisesForAbs = !isEven
 
     val message = """
-Упражнения для рук:       $isArmsAndAbsDay
-Упражнения для ног:       $isLegsAndBackDay
-Упражнения для спины:     $isArmsAndAbsDay
-Упражнения для пресса:    $isArmsAndAbsDay
+Упражнения для рук: $exercisesForArmsAndAbs
+Упражнения для ног: $exercisesForLegs
+Упражнения для спины: $exercisesForBack
+Упражнения для пресса: $exercisesForAbs
     """
-
     println(message)
 }
